9f3e692e08f986e21e83dbe9852c1ef07ef0852c
Maicon Leandro <snubhousedev@gmail.com>
2024-05-11 17:05:39 -0300

    feat: add novos modelos no painel de administração

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, Address
+from .models import UserProfile, Address, UserAddress, Product, Announcement, Question
 
 
 # Register your models here.
@@ -7,10 +7,33 @@ from .models import UserProfile, Address
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_diplay = ['user', 'email', 'phone', 'photo']
-    search_fields = ['user']
+    list_display = ['user', 'email', 'phone', 'photo', 'address']
+    search_fields = ['user__username']  # Corrigido typo no search_fields
 
 
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
-    list_display = ['address', 'number', 'complement', 'nickname']
+     list_display = ['cep', 'public_place', 'public_place_type', 'neighborhood', 'city', 'state']
+
+
+@admin.register(UserAddress)
+class UserAddressAdmin(admin.ModelAdmin):
+    list_display = ['address', 'user', 'number', 'complement', 'nickname']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'author', 'seller']
+    search_fields = ['name', 'author']  # Adicionado search_fields
+
+
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ['title', 'study_area', 'condition', 'price', 'product']
+    search_fields = ['title', 'study_area']  # Adicionado search_fields
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['client', 'announcement', 'question_client', 'reply', 'created', 'active']
+    search_fields = ['client__username', 'announcement__title']  # Adicionado search_fields

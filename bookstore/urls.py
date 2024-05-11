feffb74b378746b9db999fa573dac73d3d2b3e14
Maicon Leandro <snubhousedev@gmail.com>
2024-05-11 20:53:51 -0300

    feat: add novas urls

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_login, user_logout, profile_register, address_register, edit_profile, reset_password
+from .views import user_login, user_logout, profile_register, address_register, edit_profile, reset_password, user_address_register, create_product, create_announcement, edit_announcement
 
 urlpatterns = [
     path('auth/login/', user_login, name='user-login'),
@@ -7,6 +7,11 @@ urlpatterns = [
     path('auth/register/', profile_register, name='profile_register'),
     path('auth/register/address/', address_register, name='address_register'),
     path('auth/edit_profile/', edit_profile, name='edit_profile'),
-    path('auth/reset_password/', reset_password, name='reset_password')
+    path('auth/reset_password/', reset_password, name='reset_password'),
+    path('auth/user_address_register/', user_address_register, name='user_address_register'),
+    path('ads/create_product/', create_product, name='create_product'),
+    path('ads/create_announcement/', create_announcement, name='create_announcement'),
+    path('ads/edit_announcement/', edit_announcement, name='edit_announcement')
+
 
 ]

4eaa37012f9faa2596c53167d3f91a7c924ebbc1
Lucas Linard <lucas.linard@outlook.com>
2024-09-22 12:17:34 -0300

    refactor: separa ação de perguntar e responder

--- questions/urls.py
+++ questions/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import list_questions, create_question, delete_question
+from .views import list_questions, create_question, reply_question
 
 urlpatterns = [
     path('list/', list_questions, name='list_questions'),
     path('create/', create_question, name='create_question'),
-    path('delete/', delete_question, name='question_id'),
+    path('reply/', reply_question, name='reply_question'),
 ]

451e632d59a7bc9e59fa9fe99ffb377f99726331
Lucas Linard <lucas.linard@outlook.com>
2024-09-22 09:48:55 -0300

    remove: remove rota de user_logout

--- auths/urls.py
+++ auths/urls.py
@@ -3,7 +3,6 @@ from .views import user_login, user_logout, profile_register, address_register, 
 
 urlpatterns = [
     path('login/', user_login, name='user-login'),
-    path('logout/', user_logout, name='user_logout'),
     path('register/', profile_register, name='profile_register'),
     path('register/address/', address_register, name='address_register'),
     path('edit_profile/', edit_profile, name='edit_profile'),

3832f997ba118592cd5d938d123d1132d29202d1
Maicon Leandro <snubhousedev@gmail.com>
2024-09-28 18:01:41 -0300

    Refatorando o Cart e Cartiem

--- cart/models.py
+++ cart/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Cart(models.Model):
     client = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey('ads.Announcement', on_delete=models.CASCADE)
+    product = models.ManyToManyField('ads.Product',blank=True)
 
     class Meta:
-        ordering = ('product',)
+        ordering = ('client',)
 
     def __str__(self):
         return f"Product {self.product} added to cart"
--- cartitem/models.py
+++ cartitem/models.py
@@ -1,9 +1,11 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 class Itemcart(models.Model):
     cart = models.ForeignKey('cart.Cart', on_delete=models.CASCADE)
-    product = models.ForeignKey('ads.Announcement', on_delete=models.CASCADE, related_name='product_item')
+    product = models.ForeignKey('ads.Product', on_delete=models.CASCADE, related_name='product_item')
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)], default=None)
 
     class Meta:
         ordering = ('cart',)

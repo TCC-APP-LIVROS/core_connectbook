338c1efd64a3eaeb255e1454eafd860c20886855
Lucas Linard <lucas.linard@outlook.com>
2024-10-16 23:58:22 -0300

    Refactor list_announcement view to filter announcements by seller_id and add show_user parameter

--- auths/models.py
+++ auths/models.py
@@ -8,6 +8,7 @@ class Address(models.Model):
     neighborhood = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=2, default='BA')
+    street = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return f"{self.neighborhood}, {self.city}, {self.state} - CEP: {self.cep}"
--- auths/urls.py
+++ auths/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_login, profile_register, address_register, edit_profile, reset_password, user_address_register
+from .views import user_address_update, user_login, profile_register, address_register, edit_profile, reset_password, user_address_register, user_address_list
 
 urlpatterns = [
     path('login/', user_login, name='user-login'),
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/address/', address_register, name='address_register'),
     path('edit_profile/', edit_profile, name='edit_profile'),
     path('reset_password/', reset_password, name='reset_password'),
-    path('user_address_register/', user_address_register, name='user_address_register')
-
+    path('user_address_register/', user_address_register, name='user_address_register'),
+    path('user_address_update/<int:id>', user_address_update, name='user_address_update'),
+    path('user_address/<int:user_id>/', user_address_list, name='user_address')
 ]

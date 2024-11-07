11c545a4b8d5d7ea2f0f2e621b330d6c45f59384
Lucas Linard <87875191+lucas-linard@users.noreply.github.com>
2024-11-07 20:54:40 -0300

    Revert "correção do carrinho e do item carrinho"

--- cart/models.py
+++ cart/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class Cart(models.Model):
     client = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ManyToManyField('ads.Product',blank=True)
 
     class Meta:
         ordering = ('client',)
 
     def __str__(self):
-        return f"Carrinho de {self.client}"
+        return f"Product {self.product} added to cart"

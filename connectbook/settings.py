5a48f321e5384137c6c9adb0d4a33d98ae1fba48
Lucas Linard <lucas.linard@outlook.com>
2024-11-07 23:05:37 -0300

    final

--- connectbook/settings.py
+++ connectbook/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,7 +86,7 @@ WSGI_APPLICATION = 'connectbook.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'store_book',
+        'NAME': 'book',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',  # ou o endereço do seu servidor PostgreSQL
@@ -156,6 +157,11 @@ DEFAULT_FROM_EMAIL = 'your_email@example.com'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "us-east-1")  # Defina a região
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
